a3a7ee0df90716be2fbf216da6581223ad1de11b
Barros Pereira <MBarros@chacompanies.com>
2022-07-06 12:10:40 -0400

    made map heighter

--- dash_layout/layout_comp_central_map.py
+++ dash_layout/layout_comp_central_map.py
@@ -5,14 +5,22 @@ def get_central_map(prec_data):
     cent_fig = prec_data["multi_layer_fig"]
 
     # central_map = dcc.Graph(id='main_fig', figure=fig, style={'margin-left':'15%', "align":"left", "width":"58%", "height":"80vh" }) #style=get_content_style())
-    central_map = html.Div([dcc.Graph(id='main_fig', figure=cent_fig,
-                                      #style={'margin-left': '0%', "align": "left", "width": "100%", "height": "100%"}
-                                      )
-                                      ],
+    central_map = html.Div(
+            [
+                dcc.Graph(
+                    id='main_fig',
+                    figure=cent_fig,
+                    style={
+                        'height': '100%'
+                    }
+                    #style={'margin-left': '0%', "align": "left", "width": "100%", "height": "100%"}
+                    )
+            ],
                             # style={'position': 'fixed', 'top': 0, 'margin-left': '15%', "align": "left", "width": "58%","height": "95%"},
                             className="col-6",
                             style={
-                                "padding": "0px"
+                                "padding": "0px",
+                                "height": "100%",
                             }
                         )  # style=get_content_style())
 
